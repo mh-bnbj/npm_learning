aaeeb9e5f8de75115e197cf46bd40c9fb512f048
mh-bnbj <www.mohamadhasan65@gmail.com>
2023-06-15 12:59:10 +0330

    add e4 project_import-export

---
+++ e4_module_import-export/import-export/js/script.js
@@ -0,0 +1,13 @@
+
+// import{username as firstName , number} from "./utils.js"
+
+import * as Utils from "./utils.js"
+
+// import {double , number} from "./utils.js"
+
+// console.log(double(12))
+
+// console.log("Number: ",number);
+
+// import utilNumber , {username} from "./utils.js"
+console.log(Utils , Utils.username);
---
+++ e4_module_import-export/import-export/js/utils.js
@@ -0,0 +1,23 @@
+// export let username = "mohamad hasan";
+
+// export let number =22;
+
+// export function double(number){
+//     return number *2;
+// }
+
+// let username = "mohamad hasan";
+
+// let number = 22;
+
+// function double(number){
+//     return number*2;
+// }
+
+// export { username , double}
+
+let number = 34;
+
+export let username = "mohamad hasan";
+
+export default number;
